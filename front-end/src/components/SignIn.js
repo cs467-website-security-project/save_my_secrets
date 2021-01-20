1c826410cbb6680c3165d60bb8ac9e58ced71bfb
shylton <55864449+shylton@users.noreply.github.com>
2021-01-20 18:30:33 -0300

    Update SignIn.js
    removed commented-out code

--- front-end/src/components/SignIn.js
+++ front-end/src/components/SignIn.js
@@ -2,11 +2,8 @@ import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-// import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import Container from '@material-ui/core/Container';
@@ -65,10 +62,6 @@ export default function SignIn() {
             id="password"
             autoComplete="current-password"
           />
-          {/* <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          /> */}
           <ButtonGroup variant="contained" color="primary" className={classes.buttons}>
             <Button
               type="submit"
@@ -91,11 +84,6 @@ export default function SignIn() {
                 Forgot password?
               </Link>
             </Grid>
-            {/* <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid> */}
           </Grid>
         </form>
       </div>
