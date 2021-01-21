1774a68d5955a21726599acf87ec394ca8e478d3
shylton <55864449+shylton@users.noreply.github.com>
2021-01-21 18:05:42 -0300

    Update NavBar.js
    rm User link

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -33,11 +33,6 @@ export default function NavBar() {
               About
             </NavLink>
           </Button>
-          <Button>
-            <NavLink to="/User" className={classes.link}>
-              UserView
-            </NavLink>
-          </Button>
         </Toolbar>
       </AppBar>
     </div>
