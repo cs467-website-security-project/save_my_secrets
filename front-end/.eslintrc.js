9171eb2cf44f9ca351f3d4c12b781f8875214ccd
Chunghee Kim <chungheek@gmail.com>
2021-01-21 22:34:11 -0500

    Added more dev dependencies for linter and formatted files

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -15,12 +11,10 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': ['error'],
     'eol-last': ['error', 'always'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
   },
 };
--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -3,11 +3,11 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
 export default function About() {
-    return (
-        <Box my={4}>
-            <Typography variant="h4" component="h1" gutterBottom>
-                About Us will be here...
-            </Typography>
-        </Box>
-    );
+  return (
+    <Box my={4}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        About Us will be here...
+      </Typography>
+    </Box>
+  );
 }
