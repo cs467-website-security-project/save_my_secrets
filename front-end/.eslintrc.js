56a0f725a0956dab879c6209b7c7bdb9fd465573
Chunghee Kim <chungheek@gmail.com>
2021-01-24 16:40:40 -0500

    fixing linting issues on windows

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   plugins: ['react', 'prettier'],
   rules: {
-    'prettier/prettier': ['error'],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'eol-last': ['error', 'always'],
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
   },
