cd03908cbcf3d2b67068a017a8b432fa5a89294b
Andre Paul <andrepaul8128@gmail.com>
2021-01-30 15:41:23 -0800

    add comment

--- back-end/helpers/db-helpers.js
+++ back-end/helpers/db-helpers.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+// Note that configuration is dependent on user's mysql/db setup
 let pool = mysql.createPool({ connectionLimit : 10, host : 'localhost', database : 'test', user : 'root', password : 'root', port : '8889' });
 
 async function queryDB(query) {
