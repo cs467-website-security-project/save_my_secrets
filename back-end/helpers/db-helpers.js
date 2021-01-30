af34a5d9d46589dfff2be1249f37ad1041a23f97
Andre Paul <andrepaul8128@gmail.com>
2021-01-30 14:38:44 -0800

    refactored to take out the config file

--- back-end/helpers/db-helpers.js
+++ back-end/helpers/db-helpers.js
@@ -1,4 +1,5 @@
-const { pool } = require('../config/config');
+const mysql = require('mysql');
+let pool = mysql.createPool({ connectionLimit : 10, host : 'localhost', database : 'test', user : 'root', password : 'root', port : '8889' });
 
 async function queryDB(query) {
     try {
