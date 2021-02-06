c2445b4c83a2d9dffb9b8d86468a9ba0230344f0
Chunghee Kim <chungheek@gmail.com>
2021-02-06 12:39:47 -0500

    Created ability for user to create secrets

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -7,10 +7,9 @@ router.get("/user/:userId", async function (req, res, next) {
   try {
     const userId = req.params["userId"];
 
-    const query = 
-      `SELECT username, secret 
-      FROM Users u INNER JOIN Secrets s 
-      ON u.user_id = s.user 
+    const query = `SELECT username, secret
+      FROM Users u INNER JOIN Secrets s
+      ON u.user_id = s.user
       WHERE u.user_id = ${userId}`;
 
     const results = await queryDB(query);
@@ -21,7 +20,27 @@ router.get("/user/:userId", async function (req, res, next) {
       return res.status(200).send(results);
     }
   } catch (err) {
-    console.log('user.js API ERROR:', err);
+    console.log("user.js API ERROR:", err);
+    res.status(500).send(err);
+  }
+});
+
+router.post("/user/secret", async function (req, res) {
+  try {
+    const { secret, userId } = req.body;
+
+    const query = `INSERT INTO Secrets (secret, user)
+      VALUES ('${secret}', ${userId})`;
+
+    const results = await queryDB(query);
+
+    if (results.affectedRows === 1) {
+      return res.status(200).send("SUCCESS");
+    } else {
+      return res.status(401).send("401");
+    }
+  } catch (err) {
+    console.log("user.js API ERROR:", err);
     res.status(500).send(err);
   }
 });
