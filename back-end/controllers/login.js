92d956cf0a01a7a31d611f226c5118588bb8bc43
Chunghee Kim <chungheek@gmail.com>
2021-03-04 13:55:40 -0500

    Added middleware to stop bruteforce attacks in backend

--- back-end/controllers/login.js
+++ back-end/controllers/login.js
@@ -2,9 +2,40 @@ const express = require("express");
 const CryptoJS = require("crypto-js");
 const router = express.Router();
 const { queryDB } = require("../helpers/db-helpers");
+const ExpressBrute = require("express-brute");
+
+// Since this is simple site we will use the local memory store
+// But for further development a robust memory store is required
+// https://github.com/AdamPflug/express-brute
+const store = new ExpressBrute.MemoryStore();
+
+const failCallback = (req, res) => {
+  res
+    .status(403)
+    .send(
+      "Error, You've made too many failed attempts in a short period of time, please try again."
+    );
+  res.redirect("/");
+};
+
+const handleStoreError = (error) => {
+  log.error(error);
+  throw {
+    message: error.message,
+    parent: error.parent,
+  };
+};
+
+const bruteforce = new ExpressBrute(store, {
+  freeRetries: 10,
+  minWait: 5 * 60 * 1000, // 5 minutes
+  maxWait: 60 * 60 * 1000, // 1 hour,
+  failCallback: failCallback,
+  handleStoreError: handleStoreError,
+});
 
 // Existing user
-router.post("/login", async function (req, res, next) {
+router.post("/login", bruteforce.prevent, async function (req, res, next) {
   try {
     const { username, password } = req.body;
 
@@ -21,7 +52,7 @@ router.post("/login", async function (req, res, next) {
     if (results[0].password === hashedPwd) {
       return res.status(200).send(results[0].user_id.toString());
     } else {
-      return res.status(403).send("Bad password");
+      return res.status(401).send("Bad password");
     }
   } catch (err) {
     res.status(500).send(err);
@@ -29,7 +60,7 @@ router.post("/login", async function (req, res, next) {
 });
 
 // Register new user
-router.post("/register", async function (req, res, next) {
+router.post("/register", bruteforce.prevent, async function (req, res, next) {
   try {
     const { username, password, salt } = req.body;
 
